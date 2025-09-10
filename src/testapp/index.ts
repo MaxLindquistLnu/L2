48170d780c1a259d23c47dc2b893bdb8e2e415a5
Max Lindquist <ml227cu@student.lnu.se>
2025-09-10 12:09:31 +0200

    Update package.json and tsconfig.json for TypeScript support; remove old index.ts and add new testapp/index.ts

--- src/testapp/index.ts
+++ src/testapp/index.ts
@@ -1,7 +1,7 @@
-import { CaesarCipher } from "../src/CaesarCipher.js";
+import { CaesarCipher } from "../CaesarCipher.js";
 
 const cipher = new CaesarCipher(120);
-const encrypted = cipher.encrypt("Hellh, 123ld!");
+const encrypted = cipher.encrypt("Hellh, 124ld!");
 const decrypted = cipher.decrypt(encrypted);
 console.log(encrypted); // Outputs: "Rovvy, 123vf!"
 console.log(decrypted); // Outputs: "Hellh, 123ld!"
